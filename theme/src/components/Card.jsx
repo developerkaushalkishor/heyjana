06d29f97b61797e374c755ead9a4129490ba62d7
kaushalkishor97200@gmail.com <kaushalkishor97200@gmail.com>
2024-10-15 22:00:58 +0530

    updated commit

--- theme/src/components/Card.jsx
+++ theme/src/components/Card.jsx
@@ -7,7 +7,7 @@ import {
 import ContentAdder from "./ContentAdder";
 
 const Card = () => {
-  const [card, setCard] = useState([
+  const [content, setContent] = useState([
     {
       id: 1,
       data: `Dil ki 💓 dhadkan mein tum, saanson 🌬️ mein ho base, ❤️ Ishq 💕 ki
@@ -18,7 +18,7 @@ const Card = () => {
     <div>
       <div className="sm:mb-8 sm:flex sm:justify-center my-5 sm:my-5 lg:my-10 flex justify-center align-middle flex-col">
         <div className="relative rounded-full px-6 py-3 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20 text-center">
-          {card.map((data) => data.data)}
+          {content.map((data) => data.data)}
         </div>
         <div className="flex ml-6 bottom-3 relative gap-3 text-gray-700">
           <PencilSquareIcon className="h-5 cursor-pointer" />
